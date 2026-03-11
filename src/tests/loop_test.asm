3dbecbb1eb2366c8573426c6b0d16ab2fd127fca
Minhaj <mdminhaju29@gmail.com>
2026-03-11 08:10:51 +0530

    Modified test case of loop

--- src/tests/loop_test.asm
+++ src/tests/loop_test.asm
@@ -5,31 +5,7 @@ ldc 0           ; Initialize sum = 0
 stl 0           ; sum at [SP+0]
 
 ldc 10          ; Initialize counter = 10
-loop:
-    ldl 0       ; sum -> A, counter -> B
-    add         ; sum = sum + counter
-    stl 0       ; store new sum
-    
-    ldc -1
-    adc 0       ; subtract 1 from counter? no, adc adds to A
-    ; correct way to decrement B(counter):
-    ; A is sum, B is counter
-    ; load counter, sub 1, branch
-    
-    ldl 0       ; A = sum
-    sp2a        ; A = SP, B = sum
-    ldc 1       ; A = 1, B = SP
-    add         ; A = SP+1
-    ; wait, this ISA is tricky. 
-    ; Let's re-do simple loop.
-    
-    ; SP+0: sum
-    ; SP+1: counter
-    
-    ldc 0
-    stl 0       ; sum=0
-    ldc 10
-    stl 1       ; counter=10
+stl 1
     
 loop_start:
     ldl 1       ; A=counter
